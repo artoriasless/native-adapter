e5fd33ffd0346dae871f549e30c16ecebb0dc04f
artoriasless <tangjianyi1993@live.com>
2022-09-21 22:55:14 +0800

    init: init repo

---
+++ invoke_test/InvokeTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace invoke_test
+{
+    public class InvokeTest
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("invoke test start.....");
+
+            // invoke some dll...
+            InvokeSample();
+
+            Console.WriteLine("invoke test end.....");
+            Console.WriteLine("----------------------------------");
+        }
+        public static void InvokeSample()
+        {
+            Console.WriteLine("invoke sample here.....");
+        }
+    }
+}
